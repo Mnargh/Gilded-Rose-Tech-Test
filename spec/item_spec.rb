7a045197bfa58e21a3a9fbf01d1f0080ba6986fb
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-30 11:39:40 +0000

    Created test for Item initialisation. Transferred Item class in to it's own file item.rb

---
+++ spec/item_spec.rb
@@ -0,0 +1,11 @@
+require './lib/item'
+
+describe Item do
+
+  let(:vest){Item.new("+ 5 Dexterity Vest", 10, 20)}
+  it "Should initialise with name, sell_in, and quality properties" do
+    expect(vest.name).to eq "+ 5 Dexterity Vest"
+    expect(vest.sell_in).to eq 10
+    expect(vest.quality).to eq 20
+  end
+end
