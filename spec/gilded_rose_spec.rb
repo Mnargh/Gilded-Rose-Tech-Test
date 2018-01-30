3c98b725605946d05fb9e6e501a43ed0a278a86b
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-30 12:29:54 +0000

    Added tests for accessing items and item quality decreasing

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,11 +2,19 @@ require './lib/gilded_rose'
 
 describe GildedRose do
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
+  describe "Update_quality" do
+    it "Can access the items stored" do
+      items = [Item.new("+5 Dexterity Vest", 10, 20)]
+      GildedRose.new(items)
+      expect(items[0].sell_in).to eq 10
+      expect(items[0].quality).to eq 20
+    end
+
+    it "Item quality decreases by 1 every day" do
+      items = [Item.new("+5 Dexterity Vest", 10, 20)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].sell_in).to eq 9
+      expect(items[0].quality).to eq 19
     end
   end
 
