6381e9795e3fa9d9caf96cd147e7e1a9402bf2ad
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-30 11:42:39 +0000

    Added test for Item conversion of properties to a string

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,10 +2,15 @@ require './lib/item'
 
 describe Item do
 
-  let(:vest){Item.new("+ 5 Dexterity Vest", 10, 20)}
+  let(:vest){Item.new("+5 Dexterity Vest", 10, 20)}
+
   it "Should initialise with name, sell_in, and quality properties" do
-    expect(vest.name).to eq "+ 5 Dexterity Vest"
+    expect(vest.name).to eq "+5 Dexterity Vest"
     expect(vest.sell_in).to eq 10
     expect(vest.quality).to eq 20
+  end
+
+  it "Should convert it's properties to a string" do
+    expect(vest.to_s).to eq "+5 Dexterity Vest, 10, 20"
   end
 end
