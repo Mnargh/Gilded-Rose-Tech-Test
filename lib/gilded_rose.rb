c2b9261ae4f77b50d87c8ddb35b9cefad7c32776
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-30 16:25:17 +0000

    Refactored names of items for clarity

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,42 +1,48 @@
 class GildedRose
 
+  BRIE = "Aged Brie"
+  PASS = "Backstage passes to a TAFKAL80ETC concert"
+  SULFURAS = "Sulfuras, Hand of Ragnaros"
+
   def initialize(items)
     @items = items
   end
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            decrease_quality(item, 1)
+      if item.name != BRIE and item.name != PASS
+          if item.quality > 0
+              if item.name != SULFURAS
+                decrease_quality(item, 1)
+              end
           end
-        end
       else
-        if item.quality < 50
-          increase_quality(item, 1)
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                increase_quality(item, 1)
+          if item.quality < 50
+            increase_quality(item, 1)
+              if item.name == PASS
+                  if item.sell_in < 11
+                      if item.quality < 50
+                        increase_quality(item, 1)
+                      end
+                  end
+                  if item.sell_in < 6
+                      if item.quality < 50
+                        increase_quality(item, 1)
+                      end
+                  end
               end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                increase_quality(item, 1)
-              end
-            end
           end
-        end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
+
+
+      if item.name != SULFURAS
         decrease_sell_in(item, 1)
       end
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.name != BRIE
+          if item.name != PASS
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
+              if item.name != SULFURAS
                 decrease_quality(item, 1)
               end
             end
