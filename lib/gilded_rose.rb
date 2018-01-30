92b8598e4c8f75e9bfa9924ee3fb90240bccbcd1
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-30 17:29:57 +0000

    Refactored only increase exlcusion

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,45 +6,45 @@ class GildedRose
 
   def initialize(items)
     @items = items
+    @only_increase = [BRIE, PASS]
   end
 
   def update_quality()
     @items.each do |item|
+        return if item.name == SULFURAS
 
-      if item.name != BRIE and item.name != PASS
-          decrease_quality(item, 1)
-      else
-          increase_quality(item, 1)
-            if item.name == PASS
-                if item.sell_in < 11
-                      increase_quality(item, 1)
-                end
-                if item.sell_in < 6
-                      increase_quality(item, 1)
-                end
-            end
-      end
-
-      if item.name != SULFURAS
-        decrease_sell_in(item, 1)
-      end
-
-      if item.sell_in < 0
-        if item.name != BRIE
-          if item.name != PASS
-                decrease_quality(item, 1)
+          unless @only_increase.include?(item.name)
+              decrease_quality(item, 1)
           else
-            item.quality = 0
+              increase_quality(item, 1)
+                if item.name == PASS
+                    if item.sell_in < 11
+                          increase_quality(item, 1)
+                    end
+                    if item.sell_in < 6
+                          increase_quality(item, 1)
+                    end
+                end
           end
-        else
-            increase_quality(item, 1)
-        end
-      end
+
+          decrease_sell_in(item, 1)
+
+          if item.sell_in < 0
+            if item.name != BRIE
+              if item.name != PASS
+                    decrease_quality(item, 1)
+              else
+                item.quality = 0
+              end
+            else
+                increase_quality(item, 1)
+            end
+          end
     end
   end
 
   def decrease_quality(item, amount)
-    item.quality -= amount unless item.name == SULFURAS || item.quality == 0
+    item.quality -= amount unless item.quality == 0
   end
 
   def increase_quality(item, amount)
