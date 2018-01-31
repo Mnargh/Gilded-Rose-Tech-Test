b9fe94f7ee666381ac5d4a627c301e4d3bf22f0e
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-31 11:13:18 +0000

    Reset the update_quality function to its original. Added more tests to cover more options for all items

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,62 +1,54 @@
 class GildedRose
 
-  BRIE = "Aged Brie"
-  PASS = "Backstage passes to a TAFKAL80ETC concert"
-  SULFURAS = "Sulfuras, Hand of Ragnaros"
-
   def initialize(items)
     @items = items
-    @only_increase = [BRIE, PASS]
-  end
-
-  def next_day
-    items.each do |item|
-      update_quality(item)
-    end
   end
 
   def update_quality()
     @items.each do |item|
-      return if item.name == SULFURAS
-      quality_increase_over_time?(item) ? item_matures(item) : item_decays(item)
-      decrease_sell_in(item, 1)
+      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.quality > 0
+          if item.name != "Sulfuras, Hand of Ragnaros"
+            item.quality = item.quality - 1
+          end
+        end
+      else
+        if item.quality < 50
+          item.quality = item.quality + 1
+          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+            if item.sell_in < 11
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
+            end
+            if item.sell_in < 6
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
+            end
+          end
+        end
+      end
+      if item.name != "Sulfuras, Hand of Ragnaros"
+        item.sell_in = item.sell_in - 1
+      end
+      if item.sell_in < 0
+        if item.name != "Aged Brie"
+          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+            if item.quality > 0
+              if item.name != "Sulfuras, Hand of Ragnaros"
+                item.quality = item.quality - 1
+              end
+            end
+          else
+            item.quality = item.quality - item.quality
+          end
+        else
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+      end
     end
   end
-
-  def item_decays(item)
-    decrease_quality(item, 1)
-  end
-
-  def item_matures(item)
-    increase_quality(item, 1)
-    backstage_pass_bonus(item) if item.name == PASS
-  end
-
-  def backstage_pass_bonus(item)
-    increase_quality(item, 1) if item.sell_in < 11
-    increase_quality(item, 1) if item.sell_in < 6
-    item.quality = 0 if item_out_of_date?(item)
-  end
-
-  def quality_increase_over_time?(item)
-    @only_increase.include?(item.name)
-  end
-
-  def item_out_of_date?(item)
-    item.sell_in < 0
-  end
-
-  def decrease_quality(item, amount)
-    amount *= 2 if item_out_of_date?(item)
-    item.quality -= amount unless item.quality == 0
-  end
-
-  def increase_quality(item, amount)
-    item.quality += amount
-    item.quality = 50 if item.quality > 50
-  end
-
-  def decrease_sell_in(item, amount)
-    item.sell_in -= amount
-  end
-end 
+end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,22 +4,14 @@ describe GildedRose do
 
   describe "Update_quality" do
 
-    it "Can access the items stored" do
-      items = [Item.new("+5 Dexterity Vest", 10, 20)]
-      GildedRose.new(items)
-      expect(items[0].sell_in).to eq 10
-      expect(items[0].quality).to eq 20
-    end
+    # it "Can access the items stored" do
+    #   items = [Item.new("+5 Dexterity Vest", 10, 20)]
+    #   GildedRose.new(items)
+    #   expect(items[0].sell_in).to eq 10
+    #   expect(items[0].quality).to eq 20
+    # end
 
-    it "The Quality of an item is never over 50" do
-      items = [Item.new("Aged Brie", 5, 49)]
-      # add backtage passes to items list here
-      gr = GildedRose.new(items)
-      gr.update_quality()
-      expect(items[0].quality).to eq 50
-      gr.update_quality()
-      expect(items[0].quality).to eq 50
-    end
+
 
     context "Normal items" do
       it "Ordinary item quality and sell_in decreases by 1 every day" do
@@ -37,23 +29,47 @@ describe GildedRose do
       end
 
       it "Item quality decreases twice as fast once the sell by date has passed" do
-        items = [Item.new("+5 Dexterity Vest", 0, 20)]
+        items = [Item.new("+5 Dexterity Vest", 1, 20)]
         gr = GildedRose.new(items)
         gr.update_quality()
-        expect(items[0].sell_in).to eq -1
+        expect(items[0].sell_in).to eq 0
         expect(items[0].quality).to eq 19
         gr.update_quality()
-        expect(items[0].sell_in).to eq -2
+        expect(items[0].sell_in).to eq -1
         expect(items[0].quality).to eq 17
       end
     end
 
     context "Aged Brie" do
+
+      it "Sell_in value decreases by 1 every day" do
+        items = [Item.new("Aged Brie", 5, 20)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 4
+      end
+
       it "Aged Brie increases in quality the older it gets" do
         items = [Item.new("Aged Brie", 5, 20)]
         GildedRose.new(items).update_quality()
         expect(items[0].sell_in).to eq 4
         expect(items[0].quality).to eq 21
+      end
+
+      it "Aged Brie icnreases in quality twice as much after the sell_in date has passed" do
+        items = [Item.new("Aged Brie", 0, 20)]
+        gr = GildedRose.new(items)
+        gr.update_quality
+        expect(items[0].sell_in).to eq -1
+        expect(items[0].quality).to eq 22
+      end
+      it "The Quality of an item is never over 50" do
+        items = [Item.new("Aged Brie", 5, 49)]
+        # add backtage passes to items list here
+        gr = GildedRose.new(items)
+        gr.update_quality()
+        expect(items[0].quality).to eq 50
+        gr.update_quality()
+        expect(items[0].quality).to eq 50
       end
     end
 
@@ -73,21 +89,19 @@ describe GildedRose do
     end
 
     context "backstage Passes" do
+
+      it "Backstage Passes quality cannot exceed 50" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 50)]
+        gr = GildedRose.new(items)
+        gr.update_quality
+        expect(items[0].quality).to eq 50
+      end
+
       it "Backstage Passes increase in quality by 1 each day, with more than 10 days till sell_in date" do
         items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 5)]
         gr = GildedRose.new(items)
         gr.update_quality
         expect(items[0].quality).to eq 6
-      end
-
-      it "Backstage Passes increase in quality by 2 each day when between 10 and 5 days till sell_in date" do
-        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 5)]
-        gr = GildedRose.new(items)
-        gr.update_quality
-        expect(items[0].quality).to eq 7
-        4.times{gr.update_quality}
-        expect(items[0].sell_in).to eq 5
-        expect(items[0].quality).to eq 15
       end
 
       it "Backstage Passes increase in quality by 2 each day when between 10 and 5 days till sell_in date" do
@@ -111,31 +125,45 @@ describe GildedRose do
       end
 
       it "Backstage Passes decrease to 0 in quality after the sell_in date has passed" do
-        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 5)]
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 5)]
         gr = GildedRose.new(items)
         gr.update_quality
-        expect(items[0].sell_in).to eq -1
+        expect(items[0].sell_in).to eq 0
         expect(items[0].quality).to eq 8
         gr.update_quality
-        expect(items[0].sell_in).to eq -2
+        expect(items[0].sell_in).to eq -1
         expect(items[0].quality).to eq 0
 
       end
     end
 
     context "Conjured items" do
-      # it "Conjured items degrade in quality twice as fast as normal items before sell_in date" do
-      #   items = [Item.new("Conjured Mana Cake", 5, 30)]
-      #   GildedRose.new(items).update_quality
-      #   expect(items[0].quality).to eq 28
-      # end
 
-        # it "Conjured items degrade in quality twice as fast as normal items after sell_in date" do
-        #   items = [item.new("Conjured Mana Cake", 0, 30)]
-        #   GildedRose.new(items).update_quality
-        #   expect(items[0].quality).to 24
-        # end
-    end
+      skip "Conjured items decrese sell_in by 1 after each day has passed" do
+        items = [Item.new("Conjured Mana Cake", 5, 10)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq 4
+      end
+
+      skip "Conjured items' quality cannot decrease below 0" do
+        items = [Item.new("Conjured Mana Cake", 5, 0)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq 4
+        expect(items[0].quality).to eq 0
+      end
+
+      skip "Conjured items degrade in quality twice as fast as normal items before sell_in date" do
+        items = [Item.new("Conjured Mana Cake", 5, 30)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 28
+      end
+
+      skip "Conjured items degrade in quality twice as fast as normal items after sell_in date" do
+        items = [item.new("Conjured Mana Cake", 0, 30)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to 24
+      end
+  end
 
   end
 
