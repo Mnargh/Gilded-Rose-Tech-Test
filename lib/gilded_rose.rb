d7b69c47c353c0fd4694b43c47fcb205784aa08d
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-31 11:56:11 +0000

    Refactored update_quality method in to case when statement

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,22 +10,16 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-
-      if item.name == NORMAL
-        update_normal_item(item)
-      end
-
-      if item.name == SULFURAS
-        update_sulfuras(item)
-      end
-
-      if item.name == BRIE
-        update_brie(item)
-      end
-
-      if item.name == PASS
-        update_pass(item)
-      end
+      case item.name
+        when NORMAL
+          update_normal_item(item)
+        when SULFURAS
+          update_sulfuras(item)
+        when BRIE
+          update_brie(item)
+        when PASS
+          update_pass(item)
+        end
     end
   end
 
