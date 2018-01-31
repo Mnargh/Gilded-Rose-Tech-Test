18ea958aa808e324033e01ad14b6ff152c8e48d6
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-31 11:54:03 +0000

    Added code to each item update method to complete each test one at a time

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -29,4 +29,28 @@ class GildedRose
     end
   end
 
+  def update_normal_item(item)
+    item.sell_in -= 1
+    item.quality -= 1 unless item.quality == 0
+    item.quality -= 1 if item.sell_in < 0
+  end
+
+  def update_sulfuras(item)
+  end
+
+  def update_brie(item)
+    item.sell_in -= 1
+    item.quality += 1 unless item.quality == 50
+    item.quality += 1 if item.sell_in < 0
+  end
+
+  def update_pass(item)
+    item.sell_in -= 1
+    return if item.quality >= 50
+    return item.quality = 0 if item.sell_in < 0
+    item.quality += 1
+    item.quality += 1 if item.sell_in < 10
+    item.quality += 1 if item.sell_in < 5
+  end
+
 end
